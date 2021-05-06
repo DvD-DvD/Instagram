9f4c58bf3b4495d3396f47ddc00891612b12ef90
DvD-DvD <vishaaldheeraj@gmail.com>
2021-05-06 17:45:27 +0530

    login handled

--- instagram/src/pages/login.js
+++ instagram/src/pages/login.js
@@ -1,7 +1,9 @@
 import userEvent from '@testing-library/user-event';
 import { useEffect, useState, useContext } from 'react';
 import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
+import * as ROUTES from '../constants/routes';
 
 export default function Login() {
   const history = useHistory();
@@ -13,7 +15,18 @@ export default function Login() {
   const [error, setError] = useState('');
   const isInvalid = password === '' || emailAddress === '';
 
-  const handleLogin = () => {};
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push(ROUTES.DASHBOARD);
+    } catch (error) {
+      setEmailAddresss('');
+      setPassword('');
+      setError(error.message);
+    }
+  };
 
   useEffect(() => {
     document.title = 'Instagram Login';
@@ -30,32 +43,51 @@ export default function Login() {
       </div>
 
       <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center w-full">
-          <img
-            src="/images/logo.png"
-            alt="Instagram logo"
-            className="mt-2 w-6/12 mb-4"
-          />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
+        <div className="flex-col items-center bg-white p-4 border border-gray-primary mb-4 rounded">
+          <h1 className="flex justify-center w-full">
+            <img
+              src="/images/logo.png"
+              alt="Instagram logo"
+              className="mt-2 w-6/12 mb-4"
+            />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary">{error}</p>}
 
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            className="text-sm text-gray base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => console.log(target.value)}
-          />
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddresss(target.value)}
+            />
 
-          <input
-            aria-label="Enter your email password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => console.log(target.value)}
-          />
-        </form>
+            <input
+              aria-label="Enter your email password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`bg-blue-medium text-white w-full rounded h-8 font-bold
+              ${isInvalid && 'opacity-50'}`}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="flex rounded justify center items-center flex-col w-full bg-white p-4 border border-gray-primary">
+          <p className="text-sm">Don't have an account?{` `}
+            <Link
+              to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
